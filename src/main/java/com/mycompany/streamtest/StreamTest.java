ef797854c578b375ed9e4fdc6d92d253a6ef2c51
Yuki <asdfjjp@gmail.com>
2022-11-02 12:48:10 +0900

    ide commit

--- src/main/java/com/mycompany/streamtest/StreamTest.java
+++ src/main/java/com/mycompany/streamtest/StreamTest.java
@@ -12,6 +12,6 @@ package com.mycompany.streamtest;
 public class StreamTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!");
+        System.out.println("Hello World!!!");
     }
 }
